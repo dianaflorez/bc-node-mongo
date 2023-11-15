047ebad43e2e2070eb370e4fdb6fd733aa57e889
Pablo Fajardo <pablofajardoruano@gmail.com>
2023-11-15 16:38:21 -0500

    crud productos

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { Db } from 'mongodb';
+
+import config from './config';
 
 @Injectable()
 export class AppService {
+  constructor(
+    // @Inject('API_KEY') private apiKey: string,
+    @Inject('TASKS') private tasks: any[],
+    @Inject('MONGO') private database: Db,
+    @Inject(config.KEY) private configService: ConfigType<typeof config>,
+  ) {}
   getHello(): string {
-    return 'Hello World!';
+    const apiKey = this.configService.apiKey;
+    const name = this.configService.database.name;
+    return `Hello World! ${apiKey} ${name}`;
+  }
+  getTasks() {
+    const tasksCollection = this.database.collection('tasks');
+    return tasksCollection.find().toArray();
   }
 }
---
+++ src/products/services/products.service.ts
@@ -0,0 +1,44 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { Product } from './../entities/product.entity';
+import { CreateProductDto, UpdateProductDto } from './../dtos/products.dtos';
+
+@Injectable()
+export class ProductsService {
+  constructor(
+    @InjectModel(Product.name) private productModel: Model<Product>,
+  ) {}
+
+  findAll() {
+    return this.productModel.find().exec();
+  }
+
+  async findOne(id: string) {
+    const product = await this.productModel.findById(id).exec();
+    if (!product) {
+      throw new NotFoundException(`Product #${id} not found`);
+    }
+    return product;
+  }
+
+  create(data: CreateProductDto) {
+    const newProduct = new this.productModel(data);
+    return newProduct.save();
+  }
+
+  update(id: string, changes: UpdateProductDto) {
+    const product = this.productModel
+      .findByIdAndUpdate(id, { $set: changes }, { new: true })
+      .exec();
+    if (!product) {
+      throw new NotFoundException(`Product #${id} not found`);
+    }
+    return product;
+  }
+
+  remove(id: string) {
+    return this.productModel.findByIdAndDelete(id);
+  }
+}
